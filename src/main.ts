28fe49cc37156efeb6598b9b097b654d5d690729
ShantYezekyan <shant.yezekyan@gmail.com>
2024-01-31 16:41:46 +0400

    refactor canvas component and add canvas container

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,22 @@ import EyeDropper from "./components/EyeDropper";
 import "./style.css";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
+const canvasContainer = document.createElement("div");
+canvasContainer.setAttribute("class", "canvas-container");
+app.appendChild(canvasContainer);
 
-const { canvas, ctx, image} = new Canvas(app, './beach.jpg');
-const {eyeDropper, zoomFactor} = new EyeDropper(app)
+const canvasModule = new Canvas(canvasContainer);
+const eyeDropperModule = new EyeDropper(canvasContainer);
+const { canvas, ctx } = canvasModule;
+const { eyeDropper, zoomFactor } = eyeDropperModule;
+
+const { width, height } = canvasContainer.getBoundingClientRect();
+canvas.width = width;
+canvas.height = height;
+
+const image = new Image();
+image.src = "./beach.jpg";
+
+image.onload = function () {
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+};
