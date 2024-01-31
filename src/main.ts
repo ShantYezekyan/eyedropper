b10d69c36e36a5bf8b3896c2e104eb4d6c55a791
ShantYezekyan <shant.yezekyan@gmail.com>
2024-01-31 16:56:21 +0400

    add functionality to display zoomed image on the eyedropper component at exact mouse position on canvas

--- src/main.ts
+++ src/main.ts
@@ -21,4 +21,27 @@ image.src = "./beach.jpg";
 
 image.onload = function () {
   ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+  eyeDropper.style.backgroundImage = `url('${canvas.toDataURL()}')`;
+  eyeDropper.style.backgroundSize = `${canvas.width * zoomFactor}px ${
+    canvas.height * zoomFactor
+  }px`;
 };
+
+function getMousePosition(canvas: HTMLCanvasElement, e: MouseEvent) {
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: e.clientX - rect.left - window.scrollX,
+    y: e.clientY - rect.top - window.scrollY,
+  };
+}
+
+function updateEyeDropperPosition(x: number, y: number) {
+  eyeDropper.style.backgroundPosition = `-${
+    x * zoomFactor - eyeDropper.offsetWidth / 2
+  }px -${y * zoomFactor - eyeDropper.offsetHeight / 2}px`;
+}
+
+canvas.addEventListener("mousemove", (e) => {
+  const { x, y } = getMousePosition(canvas, e);
+  updateEyeDropperPosition(x, y);
+});
